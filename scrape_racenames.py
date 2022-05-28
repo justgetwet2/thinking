3996ac164aab1917cb6035aaf60ce4cd4a28d3da
AkiraMac <frogcherry7@gmail.com>
2022-05-28 18:39:14 +0900

    tullys kurozukin

--- scrape_racenames.py
+++ scrape_racenames.py
@@ -1,5 +1,6 @@
 ua = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.57"}
 from bs4 import BeautifulSoup
+from datetime import datetime
 import pandas as pd
 import pickle
 import re
@@ -26,53 +27,49 @@ def get_dfs(url):
 
 if __name__ == "__main__":
 
-    srj = lambda i: str(i).rjust(2, "0")
-    # month = [("0401", "大井", "0101")] # 中止
-    def april_races():
-        month = [(f"04{srj(i+4)}", "川崎", f"01{srj(i+1)}") for i in range(5)]
-        month += [(f"04{srj(i+11)}", "船橋", f"01{srj(i+1)}") for i in range(5)]
-        month += [(f"04{srj(i+18)}", "大井", f"02{srj(i+1)}") for i in range(5)]
-        month += [(f"04{srj(i+25)}", "浦和", f"01{srj(i+1)}") for i in range(5)]
-
-        return month
-
-    def get_racename(info_url):
-        soup = get_soup(info_url)
-        number_of_horses = len(soup.select("td.titi-haha"))
-        dist_tag = soup.select_one("div#race-data01-a")
-        distance = dist_tag.select_one("a").text
-        distance = distance.strip().replace(",", "")
-        title = soup.select_one("span.race-name").text
-        title = re.sub("　| ", "", title)
-        weather = get_weather(info_url)
-        date = info_url[42:-11]
-        racename = date + " " + course + " " + race + "R" + " " + title + " "
-        racename += distance + " " + weather + " " + str(number_of_horses) + "頭"
-
-        return racename
-
-    def get_weather(info_url):
-        url = info_url[:-5] + ".do"
-        program_url = url.replace("/race_info/", "/program/")
-        soup = get_soup(program_url)
-        tag = soup.select_one("div#sts-bangumi")
-        img1, img2 = tag.select("img")
-        weat = img1.get("alt")
-        cond = img2.get("alt")
-
-        return weat.split("：")[1] + "/" + cond.split("：")[1]
-
-    races = april_races()
-    racenames = []
-    for i, (dt, course, hold) in enumerate(races):
+    s = lambda i: str(i).rjust(2, "0")
+    pasts = []
+    # pasts += [("0401", "大井", "0101")] # 中止
+    pasts += [(f"04{s(i+3)}", "川崎", f"01{s(i)}") for i in range(1, 6)]
+    pasts += [(f"04{s(i+10)}", "船橋", f"01{s(i)}") for i in range(1, 6)]
+    pasts += [(f"04{s(i+17)}", "大井", f"02{s(i)}") for i in range(1, 6)]
+    pasts += [(f"04{s(i+24)}", "浦和", f"01{s(i)}") for i in range(1, 6)]
+    pasts += [(f"05{s(i+1)}", "船橋", f"02{s(i)}") for i in range(1, 6)]
+    pasts += [(f"05{s(i+8)}", "大井", f"03{s(i)}") for i in range(1, 6)]
+    pasts += [(f"05{s(i+15)}", "川崎", f"02{s(i)}") for i in range(1, 6)]
+    pasts += [(f"05{s(i+22)}", "大井", f"04{s(i)}") for i in range(1, 6)]
+    
+    races = []
+    for i, (dt, course, hold) in enumerate(pasts):
         if i > -1:
-            for race in (srj(n) for n in range(1, 13)):
-                target = dt + course_d[course] + hold + race + ".do"
-                info_url = nankan_url + "/race_info/" + yyyy + target
-                racename = get_racename(info_url)
-                print(racename)
-                racenames.append(racename)
+            for race in (s(n) for n in range(1, 13)):
+                target = dt + course_d[course] + hold + race
+                info_url = nankan_url + "/race_info/" + yyyy + target + ".do"
+                soup = get_soup(info_url)
+                dist_tag = soup.select_one("div#race-data01-a")
+                dist = dist_tag.select_one("a").text.strip()
+                entry_df = get_dfs(info_url)[0]
 
-    filename = "./data/" + "april" + "_racenames.pickle"
+                result_url = nankan_url + "/result/" + yyyy + target + ".do"
+                dfs = get_dfs(result_url)
+                df = [df for df in dfs if df.columns[0] == "天候"][0]
+                condition = df["天候"][0] + "/" + df["馬場"][0].split()[1]
+
+                racename = course + " " + race + "R " + soup.select_one("span.race-name").text
+                racename += " " + dist + " " + str(len(entry_df)) + "頭 " + condition
+                racename += " " + target
+                
+                # m = re.match("Ｃ３\(", racename.split()[2])
+                print(racename)
+                races.append(racename)
+                # odds_url = nankan_url + "/odds/" + yyyy + target + "01.do"
+                # odds_dfs = get_dfs(odds_url)
+                # odds_df = [df for df in odds_dfs if df.columns[0] == "枠番"][0]
+
+                # result_df = dfs[0]
+
+                # races.append((racename, entry_df, odds_df, result_df))
+        
+    filename = "./data/" + "racenames_22.pickle"
     with open(filename, "wb") as f:
-        pickle.dump(racenames, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(races, f, pickle.HIGHEST_PROTOCOL)
