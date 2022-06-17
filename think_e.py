db13798f6a2afad1b3a490a0168565b7a629696f
AkiraMac <frogcherry7@gmail.com>
2022-06-17 16:10:16 +0900

    next hakohige

--- think_e.py
+++ think_e.py
@@ -1,5 +1,7 @@
 ua = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.57"}
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "Hiragino sans"
 import pandas as pd
 import pickle
 import requests
@@ -22,10 +24,39 @@ def get_dfs(url):
             print(f"It's no table! {url}")
     return dfs
 
+def float_time(s):
+    if ":" in s:
+        a, b = s.split(":")
+        ftime = int(a) * 60 + float(b)
+    else:
+        ftime = float(s)
+    return ftime
+
+def str_time(f):
+    stime = ""
+    if f:
+        if f > 60.0:
+            sec = f - 60.0
+            stime = "1:" + str(round(sec, 1))
+        elif f <= 60.0:
+            stime = str(round(f, 1))
+    return stime
+
+def get_time(condition, stime):
+    ftime = None
+    if condition == "良":
+        ftime = float_time(stime)
+    if condition == "稍重":
+        ftime = float_time(stime) + 0.2
+    if condition == "重":
+        ftime = float_time(stime) + 0.1
+    return ftime
+
 if __name__ == "__main__":
 
     nankan_url =  "https://www.nankankeiba.com"
     yyyy = "2022"
+    JRA = "札幌", "函館", "福島", "新潟", "中山", "東京", "中京", "京都", "阪神", "小倉"
 
     filename = "./data/races_2022.pickle"
     with open(filename, mode="rb") as f:
@@ -33,26 +64,54 @@ if __name__ == "__main__":
 
     dic = {}
     for i, race in enumerate(races):
-        if i: continue
+        if i != len(races) - 1: continue
+        print(race)
         entry_url = nankan_url + "/race_info/" + yyyy + race[-1] + ".do"
         soup = get_soup(entry_url)
         tbl = soup.select_one("table.tb01")
         tags = tbl.select("a.tx-mid")
+        data = []
+        data_y = []
         for j, tag in enumerate(tags):
-            if j: continue
+            # if j > 1: continue
             uma_name = tag.text
+            print("***", uma_name)
             uma_ulr = tag.get("href")
             url = nankan_url + uma_ulr
             dfs = get_dfs(url)
             for df in dfs:
+                x, y = [], []
                 if df.columns[0] == "年月日":
                     for idx, row in df.iterrows():
                         # if idx: continue
+                        if type(row["人気"]) == float: # nan
+                            continue
+                        # if row["場名"] in JRA:
+                        #     continue
                         racename = row["レース名"]
                         course = row["場名"]
-                        dist = row["距離"]
-                        time = row["タイム"]
-                        cond = row["天候  馬場"]
+                        distance = int(row["距離"].split("m")[0])
+
+                        stime = row["タイム"]
+                        condition = row["天候  馬場"].split("/")[1]
+                        ftime = get_time(condition, stime)
+                        if not ftime:
+                            continue
                         fav = row["人気"].replace(" ", "")
                         odr = "".join(row["着/頭数"].split()[:2])
-                        print(odr, fav, dist, time, cond, course, racename)
+                        # print(odr, fav, distance, stime, condition, course, racename)
+                        if len(x) > 20:
+                            continue
+                        x.append(distance)
+                        y.append(ftime)
+                        if 1600 <= distance and distance <= 1650:
+                            data_y.append(ftime)
+            data.append((x, y))
+
+    # plt.title(uma_name)
+    fig, ax = plt.subplots()
+    ax.set_ylim([min(data_y), max(data_y)])
+    ax.set_xlim([1200, 1800])
+    for x, y in data:
+        ax.plot(x, y, ".")
+    plt.show()
