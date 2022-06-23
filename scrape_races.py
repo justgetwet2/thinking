5ef0a857ca5933b49a0eeffdc89f1da0515391be
AkiraMac <frogcherry7@gmail.com>
2022-06-23 18:56:58 +0900

    tulll

--- scrape_races.py
+++ scrape_races.py
@@ -1,5 +1,6 @@
 ua = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.57"}
 from bs4 import BeautifulSoup
+import datetime
 import pandas as pd
 import pickle
 import re
@@ -57,6 +58,7 @@ def race_tuples():
     # races += [(f"05{strj(i+29)}", "浦和", f"02{strj(i)}") for i in range(1, 3)]
     # races += [(f"06{strj(i-2)}", "浦和", f"02{strj(i)}") for i in range(3, 6)]
     # races += [(f"06{strj(i+5)}", "大井", f"05{strj(i)}") for i in range(1,  6)]
+    races += [(f"06{strj(i+19)}", "船橋", f"03{strj(i)}") for i in range(1, 6)]
 
     return races
 
@@ -66,31 +68,45 @@ if __name__ == "__main__":
     yyyy = "2022"
     course_d = { "浦和": "18", "船橋": "19", "大井": "20", "川崎": "21" }
 
-    filename = "./data/" + "races_2022.pickle"
-    with open(filename, "rb") as f:
-        read_data = pickle.load(f)
+    # filename = "./data/" + "races_2022.pickle"
+    # with open(filename, "rb") as f:
+    #     read_data = pickle.load(f)
 
     races = race_tuples()
 
     data = []
     for i, (dt, course, hold) in enumerate(races):
-        if i: continue
+        # if i: continue
+
+        top_url = nankan_url + "/program/" + yyyy + dt + course_d[course] + hold + ".do"
+        top_df = get_dfs(top_url)[0]
+        start_time_sr = top_df["発走時刻"]
+        # print(start_time_sr)
+
         for n in range(1, 13):
             race = str(n).rjust(2, "0")
             target_code = dt + course_d[course] + hold + race
 
-            result_url = nankan_url + "/result/" + yyyy + target_code + ".do"
-            dfs = get_dfs(result_url)
-            if not dfs: # レースなし
-                print(f"{dt} {race}R is nothing..")
-                continue
-            else:
-                df = dfs[0]
-                if not df.columns[0] == "着": # 結果なし
-                    print(f"{dt} {race}R has no result..")
-                    continue
-            df = [df for df in dfs if df.columns[0] == "天候"][0]
-            condition = df["天候"][0] + "/" + df["馬場"][0].split()[1]
+            condition = ""
+            race_datetime = datetime.datetime.strptime(yyyy + dt, "%Y%m%d")
+            timedelta = datetime.datetime.now() - race_datetime
+            # print(timedelta.days)
+            if timedelta.days > 0:
+                result_url = nankan_url + "/result/" + yyyy + target_code + ".do"
+                dfs = get_dfs(result_url)
+                if not dfs: # レースなし
+                    print(f"{dt} {race}R is nothing..")
+                    # continue
+                else:
+                    df = dfs[0]
+                    if not df.columns[0] == "着": # 結果なし
+                        print(f"{dt} {race}R has no result..")
+                        # continue
+                df = [df for df in dfs if df.columns[0] == "天候"][0]
+                condition = df["天候"][0] + "/" + df["馬場"][0].split()[1]
+
+            mmdd = dt[:2] + "/" + dt[2:]
+            raceno = race + "R"
 
             info_url = nankan_url + "/race_info/" + yyyy + target_code + ".do"
             soup = get_soup(info_url)
@@ -98,22 +114,31 @@ if __name__ == "__main__":
             racename = re.sub("\u3000", " ", racename).strip()
 
             tag = soup.select_one("div#race-data01-a")
-            dist = tag.select_one("a").text.strip()
-            dist = dist.strip("ダ")
-            
-            entry_df = get_dfs(info_url)[0]
-            head_count = str(len(entry_df)) + "頭"
-            
-            mmdd = dt[:2] + "/" + dt[2:]
-            raceno = race + "R"
-            t = mmdd, course, raceno, racename, dist, head_count, condition, target_code
-            print(t)
+            distance = tag.select_one("a").text.strip()
+            distance = distance.strip("ダ")
+
+            info_df = get_dfs(info_url)[0]
+            head_count = str(len(info_df)) + "頭"
+
+            start_time = start_time_sr[n-1]
+
+            wakuban_sr = info_df["枠\u0020\u0020番"]
+            wakuban_sr.name = "枠番"
+            umaban_sr = info_df["馬\u0020\u0020番"]
+            umaban_sr.name = "馬番"
+            names = [nb.split()[0] for nb in info_df["馬名\u0020\u0020生年月日"]]
+            name_sr = pd.Series(names, name="馬名")
+            entry_df = pd.concat([wakuban_sr, umaban_sr, name_sr], axis=1)
+            # print(entry_df)
+
+            t = mmdd, course, raceno, racename, distance, head_count, start_time, condition, target_code, entry_df
+            print(" ".join(t[:-2]))
             data.append(t)
 
     print(f"{len(data)} races are created.")
     # data = read_data + data
 
-    # filepath = "./data/" + "races_2022.pickle"
-    # with open(filepath, "wb") as f:
-    #     pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+    filepath = "./data/" + "test_2022.pickle"
+    with open(filepath, "wb") as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 
